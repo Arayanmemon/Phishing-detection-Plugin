f2179b1c52db46289d2dae25beb67625ac6e1052
fahad <fahadzardari111@gmail.com>
2024-04-29 00:47:21 +0500

    added code for model probability

--- src/App.jsx
+++ src/App.jsx
@@ -4,45 +4,27 @@ import axios from 'axios';
 function App() {
   const myRef = useRef(null);
   const [info, setInfo] = useState("")
-  const [previousUrl, setPreviousUrl] = useState("")
-  useEffect(() => {
-    chrome.storage.local.get(['url'], (result) => {
-      console.log("result")
-      if (result.url) {
-        setInfo(result.url);
-      }
-    });
+  const [currentTab, setCurrentTab] = useState(1);
+  const [currentUrl, setCurrentUrl] = useState("");
+  const [load, setLoad] = useState(false);
+  const [show, setShow] = useState(false);
 
-    const tabUpdatedHandler = (tabId, changeInfo, tab) => {
-      if (changeInfo.status === 'complete' && tab.url !== info) {
-        setInfo(tab.url);
-        chrome.storage.local.set({ url: tab.url });
-      }
-    };
-
-    chrome.tabs.onUpdated.addListener(tabUpdatedHandler);
-
-    return () => {
-      chrome.tabs.onUpdated.removeListener(tabUpdatedHandler);
-    };
-  }, []);
-  const oneclick = async () => {
+  const checkSpam = async () => {
+    setLoad(true);
     let [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
     console.log([tab][0].url);
-    setInfo([tab][0].url);
+    setCurrentUrl([tab][0].url);
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
       func: () => {
+        URL
 
       }
     })
-  }
-
-  const checkSpam = async () => {
     let config = {
       method: 'get',
       maxBodyLength: Infinity,
-      url: `https://www.ipqualityscore.com/api/json/url/VCAwWHMpf8tBh97hTODyAxEgbcKNBDxp/${encodeURIComponent(info)}`,
+      url: `https://www.ipqualityscore.com/api/json/url/VCAwWHMpf8tBh97hTODyAxEgbcKNBDxp/${encodeURIComponent([tab][0].url)}`,
       headers: {}
     };
 
@@ -50,39 +32,124 @@ function App() {
       .then((response) => {
         console.log(JSON.stringify(response.data.risk_score));
         // alert(JSON.stringify(response.data.risk_score))
-        myRef.current.style.transform = `rotate(${response.data.risk_score*1.8}deg)`;
+        setLoad(false);
+        setShow(true);
+        myRef.current.style.transform = `rotate(${response.data.risk_score * 1.8}deg)`;
         setInfo(JSON.stringify(response.data.risk_score));
+        // myRef.current.style.transform = `rotate(${info * 1.8}deg)`;
+
       })
       .catch((error) => {
         console.log(error);
       });
+    myRef.current.style.transform = `rotate(${info * 1.8}deg)`;
 
   }
 
+  const checkProbability = async () => {
+    setLoad(true);
+    let [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
+    console.log([tab][0].url);
+    setCurrentUrl([tab][0].url);
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      func: () => {
+        URL
+
+      }
+    })
+    let config = {
+      method: 'get',
+      url: `http://127.0.0.1:4444/predict?url=${encodeURIComponent([tab][0].url)}`,
+    };
+
+    await axios.request(config)
+      .then((response) => {
+        console.log(JSON.stringify(response.data.probability));
+        // alert(JSON.stringify(response.data.risk_score))
+        setLoad(false);
+        setShow(true);
+        myRef.current.style.transform = `rotate(${Math.floor(response.data.probability * 100 * 1.8)}deg)`;
+        setInfo(JSON.stringify(Math.floor(response.data.probability * 100)));
+        // // myRef.current.style.transform = `rotate(${info * 1.8}deg)`;
+
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    myRef.current.style.transform = `rotate(${Math.floor(response.data.probability * 100 * 1.8)}deg)`;
+
+  }
+
+
   return (
     <>
-      <h1>Phishing Site Detection</h1>
-      <div className="card">
-        <div className='btns'>
-          <button onClick={() => oneclick()}>
-            Check spam
-          </button>
-          <button onClick={() => checkSpam()}>
-            API Call
-          </button>
+      <h1>Phishing Risk Score</h1>
+      <div className={`card`}>
+        <div className="tabs">
+          <button className={`${currentTab === 1 ? "activeTab" : "notActiveTab"}`} onClick={() => setCurrentTab(1)}>ipqualityscore</button>
+          <button className={`${currentTab === 2 ? "activeTab" : "notActiveTab"}`} onClick={() => setCurrentTab(2)}>our ml model</button>
         </div>
-        <p>
-          Score : {info}
-        </p>
-        <p>
-          Previous Url: {previousUrl}
-        </p>
+        <div className={`${currentTab === 1 ? "show" : "hide"}`}>
+          <div className='btns'>
 
-        <div>
-          <div id="el" data-value={info}>
-            <span id="needle" ref={myRef}></span>
+            <button onClick={() => checkSpam()}>
+              Get Phishing Risk Score
+            </button>
           </div>
-          <strong>Risk Score</strong>
+          {
+            load &&
+            <div>
+              <p>Loading...</p>
+            </div>
+          }
+          {!load && show &&
+            <>
+              <p>
+                URL : {currentUrl}
+              </p>
+              <p>
+                Score : {info}
+              </p>
+
+            </>
+          }
+          <div className={`${show ? "show" : "hide"}`}>
+            <div id="el" data-value={info}>
+              <span id="needle" ref={myRef} style={{ transform: `rotate(${info * 1.8}deg)`, transitionDuration: "500ms" }}></span>
+            </div>
+            <strong>Risk Score</strong>
+          </div>
+        </div>
+        <div className={`${currentTab === 2 ? "show" : "hide"}`}>
+          <div className='btns'>
+            <button onClick={() => checkProbability()}>
+              Get Phishing Risk Probabilty from Model
+            </button>
+          </div>
+          {
+            load &&
+            <div>
+              <p>Loading...</p>
+            </div>
+          }
+          {!load && show &&
+            <>
+              <p>
+                URL : {currentUrl}
+              </p>
+              <p>
+                Score : {info}
+              </p>
+
+            </>
+          }
+          <div className={`${show ? "show" : "hide"}`}>
+            <div id="el" data-value={info}>
+              <span id="needle" ref={myRef} style={{ transform: `rotate(${info * 1.8}deg)`, transitionDuration: "500ms" }}></span>
+            </div>
+            <strong>Risk Score</strong>
+          </div>
         </div>
       </div>
     </>
